bef6579b0ddd8cd666811a687f56f8a521ad32ff
Serhii Pedko <nitrotoll@gmail.com>
2021-04-07 17:18:03 +0300

    indents

--- gsuite/data_user_values.go
+++ gsuite/data_user_values.go
@@ -1,24 +1,24 @@
 package gsuite
 
 import (
-	"fmt"
-	"strings"
+  "fmt"
+  "strings"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	directory "google.golang.org/api/admin/directory/v1"
+  "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+  directory "google.golang.org/api/admin/directory/v1"
 )
 
 func dataUserValues() *schema.Resource {
-	return &schema.Resource{
-		Read: dataUserValuesRead,
-		Schema: map[string]*schema.Schema{
-			"primary_email": {
-				Type:     schema.TypeString,
-				Required: true,
-				StateFunc: func(val interface{}) string {
-					return strings.ToLower(val.(string))
-				},
-			},
+  return &schema.Resource{
+    Read: dataUserValuesRead,
+    Schema: map[string]*schema.Schema{
+      "primary_email": {
+        Type:     schema.TypeString,
+        Required: true,
+        StateFunc: func(val interface{}) string {
+          return strings.ToLower(val.(string))
+        },
+      },
 
       "custom_schemas": {
         Type:     schema.TypeList,
@@ -41,26 +41,26 @@ func dataUserValues() *schema.Resource {
 }
 
 func dataUserValuesRead(d *schema.ResourceData, meta interface{}) error {
-	config := meta.(*Config)
+  config := meta.(*Config)
 
-	var user *directory.User
-	var err error
-	err = retry(func() error {
-		user, err = config.directory.Users.Get(d.Get("primary_email").(string)).Projection("full").Do()
-		return err
-	}, config.TimeoutMinutes)
+  var user *directory.User
+  var err error
+  err = retry(func() error {
+    user, err = config.directory.Users.Get(d.Get("primary_email").(string)).Projection("full").Do()
+    return err
+  }, config.TimeoutMinutes)
 
-	if err != nil {
-		return handleNotFoundError(err, d, fmt.Sprintf("User %q", d.Id()))
-	}
+  if err != nil {
+    return handleNotFoundError(err, d, fmt.Sprintf("User %q", d.Id()))
+  }
 
-	u_err, u_msg := flattenCustomSchema(user.CustomSchemas)
+  u_err, u_msg := flattenCustomSchema(user.CustomSchemas)
   if u_err != nil {
     return handleNotFoundError(u_err, d, fmt.Sprintf("Flatten schema error with user %q", d.Id()))
   }
 
-	d.SetId(user.Id)
-	d.Set("custom_schemas", u_msg)
+  d.SetId(user.Id)
+  d.Set("custom_schemas", u_msg)
 
-	return nil
+  return nil
 }
